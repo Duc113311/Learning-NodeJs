2233a5a87745b3424c054fa80b29416ff269b4cd
Duc Nguyen <ducnv@bachasoft.com>
2022-08-26 18:01:28 +0700

    sds

--- app/index.js
+++ app/index.js
@@ -1,1 +1,44 @@
+const yargs = require("yargs");
+
+// CRUD
+// Create
+yargs.command({
+  command: "create",
+  handler: () => {
+    console.log("create");
+  },
+});
+
+// Read-all
+yargs.command({
+  command: "read-all",
+  handler: () => {
+    console.log("read-all");
+  },
+});
+
+// Real-detail
+yargs.command({
+    command:"read-detail",
+    handler:()=>{
+        console.log("read-detail");
+    }
+})
+
+// update
+yargs.command({
+    command:"update",
+    handler:()=>{
+        console.log("update");
+    }
+})
+
+// delete
+yargs.command({
+    command:"delete",
+    handler:()=>{
+        console.log("delete");
+    }
+})
+yargs.parse();
  
